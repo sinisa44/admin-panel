e02f32a77dd9fedea683884e105f8705dd7cea60
sinisa <miskovic.sinisa44@gmail.com>
2020-05-11 16:42:51 +0200

    home ui start

--- resources/views/hostinguser/create.blade.php
+++ resources/views/hostinguser/create.blade.php
@@ -11,6 +11,29 @@
         @include( 'hostinguser.modal.domain.index')
         @include( 'hostinguser.modal.hosting.index')
         @include( 'hostinguser.modal.tech.index')
+
+        <div class="user-type--heading">
+            <h1>CHOOSE USER TYPE</h1>
+            <div class="user-type--heading__icons">
+                <div class="icon-container">
+                    <i class="fas fa-user fa-7x"></i>
+                    <span> Private Person</span>
+                </div>
+                <div class="icon-container">
+                    <i class="fas fa-user-tie fa-7x"></i>
+                    <span>Company</span>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="user-type-private">
+
+        </div>
+
+        <div class="user-type-company">
+
+        </div>
     </div>
   
     @include( 'layouts.sidenav.right.index')
